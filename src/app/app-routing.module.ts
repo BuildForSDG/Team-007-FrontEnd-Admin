a220c1b0af15d93f9972d22c775ca6c7ec0f9f1f
Ebere <ebereuzodufa@gmail.com>
2020-05-18 15:50:50 +0100

    implements basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { AdminsComponent } from './admins/admins.component';
+import { UsersComponent } from './users/users.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { ServiceProvidersComponent } from './service-providers/service-providers.component';
+import { BlogComponent } from './blog/blog.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'category',
+    component: CategoryComponent
+  },
+  {
+    path: 'admins',
+    component: AdminsComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'transactions',
+    component: TransactionsComponent
+  },
+  {
+    path: 'service-providers',
+    component: ServiceProvidersComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: '',
+    redirectTo: 'category',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
